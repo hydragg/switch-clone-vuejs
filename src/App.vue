13e7cc4db79274dd6bd17bb85b9b3ff2465ea917
Hydra <hydragg@gmail.com>
2017-06-28 22:04:49 +0800

    add navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div id="app">
+    <navbar></navbar>
     <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import navbar from '@/components/navbar'
+const url = 'https://api.twitch.tv/kraken/streams?game=League%20of%20Legends&client_id=9j9fga8sofpqh8t10cig2lihlms5sh';
+
+axios.get(url)
+.then(res => {
+  console.log(res);
+});
+
 export default {
   name: 'app',
+  components: { navbar }
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+body
+  margin: 0
+  padding: 0
+#app 
+  font-family: 'Avenir' Helvetica Arial sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
 </style>
+
---
+++ src/components/navbar.vue
@@ -0,0 +1,68 @@
+<template lang="pug">
+  #navbar
+    ul
+      li 
+        router-link(to="/") Twitch(vue clone)
+      li.drowdown
+        .dropbtn Games
+        div.dropdownContent
+          router-link(to="/") League of Legends
+          router-link(to="/") Dota2
+      li.search
+        input(type="text" placeholder="Search...")
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="sass">
+  #navbar
+    background: #8983F3
+    width: 100%
+    height: 60px
+    margin: 0
+    padding: 0
+    .drowdown
+      position: relative
+      width: 120px
+      &:hover
+        .dropdownContent
+          display: block
+      .dropbtn
+        cursor: pointer
+      .dropdownContent
+        display: none
+        position: absolute
+        width: 120px
+        z-index: 2
+        a 
+          display: block
+          font-size: 14px
+          &:hover
+            background: #eee
+    ul
+      list-style-type: none
+      width: 100%
+      height: 100%
+      margin: 0
+      padding: 0
+      .search 
+        float: right
+        input
+          height: 20px
+          margin-right: 30px
+      li
+        display: inline-block
+        font-size: 25px
+        line-height: 60px
+        margin: 0 10px
+        float: left
+        a
+          text-decoration: none
+</style>
